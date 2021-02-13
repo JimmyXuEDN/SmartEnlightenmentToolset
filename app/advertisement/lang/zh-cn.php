650d03233a66a4a9115d75c7efb301205a016983
Lucas Xu <lucas@xu.com>
2021-02-13 21:39:25 +0800

    REMOVE common version v0

--- app/advertisement/lang/zh-cn.php
+++
@@ -1,6 +0,0 @@
-<?php
-return [
-   // 验证返回的信息
-   'validate_ad_photos_require' => '图片必须',
-];
- ?>
---
+++ runtime/cache/48/e345b986ae9584f9686dac64ea2c6a.php
@@ -0,0 +1,4 @@
+<?php
+//000000000000
+ exit();?>
+a:1:{s:4:"info";a:4:{s:7:"version";s:17:"V1.0.2.1806020001";s:16:"application_name";s:9:"排排兑";s:4:"logo";s:57:"http://img.wx-qz.com/feb08da9955dcae1d6996e3ffbced493.png";s:11:"description";s:43:"SmartEnlightenmentToolset斯图兰API框架";}}
